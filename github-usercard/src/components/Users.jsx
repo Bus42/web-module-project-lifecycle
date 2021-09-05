67b110187fa29f47c1d21f44eebb0d967456d423
Greg Brewton <gbrewton1980@gmail.com>
2021-09-04 21:09:55 -0600

    arranges usercards in flexboxes

--- github-usercard/src/components/Users.jsx
+++ github-usercard/src/components/Users.jsx
@@ -1,28 +1,41 @@
-import React, { Component } from 'react';
-import ImageList from '@material-ui/core/ImageList';
-import ImageListItem from '@material-ui/core/ImageListItem';
-import ListSubheader from '@material-ui/core/ListSubheader';
-import Usercard from './Usercard'
-
+import React, { Component } from "react";
+import ImageList from "@material-ui/core/ImageList";
+import ImageListItem from "@material-ui/core/ImageListItem";
+import ListSubheader from "@material-ui/core/ListSubheader";
+import Usercard from "./Usercard";
 
 class Users extends Component {
-    constructor() {
-        super();
-        this.state={}
-    }
+  constructor() {
+    super();
+    this.state = {};
+  }
 
-    render() { 
-        return ( <div id="users_component">
-            <ImageList>
-                <ImageListItem >
-                    <ListSubheader component='div' style={{display: 'block'}} >Github Users</ListSubheader>
-                </ImageListItem>
-            {this.props.users.map((user, index) => 
-                <Usercard user={user} key={index}/>
-            )}
-            </ImageList>
-        </div> );
-    }
+  subHeaderStyle = {
+    display: "flex",
+    flexFlow: "row nowrap",
+    justifyContent: "center",
+    height: "3rem",
+    width: "100%",
+  };
+
+  render() {
+    return (
+      <div id="users_component">
+        <ImageList>
+          <div className="firstUserCard">
+            <ImageListItem style={this.subHeaderStyle}>
+              <ListSubheader component="div">Github Users</ListSubheader>
+            </ImageListItem>
+          </div>
+          <div className="userCards">
+            {this.props.users.map((user, index) => (
+              <Usercard user={user} key={index} />
+            ))}
+          </div>
+        </ImageList>
+      </div>
+    );
+  }
 }
- 
+
 export default Users;
