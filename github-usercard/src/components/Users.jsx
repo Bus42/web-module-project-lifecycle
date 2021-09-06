2485906084863ef9f69f602d50fa3800a463962b
Greg Brewton <gbrewton1980@gmail.com>
2021-09-05 22:18:08 -0600

    WIP - meets MVP

--- github-usercard/src/components/Users.jsx
+++ github-usercard/src/components/Users.jsx
@@ -5,33 +5,17 @@ import ListSubheader from "@material-ui/core/ListSubheader";
 import Usercard from "./Usercard";
 
 class Users extends Component {
-  constructor() {
-    super();
-    this.state = {};
-  }
-
-  subHeaderStyle = {
-    display: "flex",
-    flexFlow: "row nowrap",
-    justifyContent: "center",
-    height: "3rem",
-    width: "100%",
-  };
 
   render() {
     return (
       <div id="users_component">
         <ImageList>
-          <div className="firstUserCard">
-            <ImageListItem style={this.subHeaderStyle}>
-              <ListSubheader component="div">Github Users</ListSubheader>
-            </ImageListItem>
-          </div>
-          <div className="userCards">
-            {this.props.users.map((user, index) => (
-              <Usercard user={user} key={index} />
-            ))}
-          </div>
+          <ImageListItem>
+            <ListSubheader component="div">Github Users</ListSubheader>
+          </ImageListItem>
+          {this.props.users.map((user, index) => (
+            <Usercard user={user} key={index} />
+          ))}
         </ImageList>
       </div>
     );
