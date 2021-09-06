7ea4e0c1f028eb257eadd6ba7eddf240fad853c4
Greg Brewton <gbrewton1980@gmail.com>
2021-09-05 22:38:08 -0600

    Adds link to users' GitHub page in title bar

--- github-usercard/src/components/Usercard.jsx
+++ github-usercard/src/components/Usercard.jsx
@@ -1,27 +1,27 @@
-import React, { Component } from 'react';
-import ImageListItemBar from '@material-ui/core/ImageListItemBar';
-import ImageListItem from '@material-ui/core/ImageListItem';
+import React, { Component } from "react";
+import ImageListItemBar from "@material-ui/core/ImageListItemBar";
+import ImageListItem from "@material-ui/core/ImageListItem";
 
 class Usercard extends Component {
+  componentDidMount() {
+    console.log("%cUsercard: CDM", "color: green");
+  }
 
-    componentDidMount(){
-        console.log('%cUsercard: CDM', 'color: green')
-    }
+  componentDidUpdate() {
+    console.log("%cUsercard: CDU", "color: orange");
+  }
 
-    componentDidUpdate(){
-        console.log('%cUsercard: CDU', 'color: orange')
-    }
-
-    render() { 
-        return ( 
-        <ImageListItem >
-            <img src={this.props.user.avatar_url} alt={`${this.props.user.login} avatar`} />
-            <ImageListItemBar
-                title={this.props.user.login}
-                subtitle={this.props.user.name}
-            />
-        </ImageListItem> );
-    }
+  render() {
+    const { avatar_url, login, name, html_url } = this.props.user;
+    return (
+      <ImageListItem>
+        <img src={avatar_url} alt={`${login} avatar`} />
+        <a href={html_url} target={login}>
+          <ImageListItemBar title={login} subtitle={name} />
+        </a>
+      </ImageListItem>
+    );
+  }
 }
- 
+
 export default Usercard;
